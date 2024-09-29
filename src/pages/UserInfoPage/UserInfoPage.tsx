c0fd8cb110fe8869eb4724087fe2084979d420e0
Sergei <Sergei.stolpovskikh@gmail.com>
2024-09-29 21:13:44 +0300

    main, users, userPage

---
+++ src/pages/UserInfoPage/UserInfoPage.tsx
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import { USERS } from "../../data";
+import "./UserInfoPage.css";
+
+export function UserInfoPage() {
+	const { userId } = useParams();
+	const user = USERS[Number(userId)];
+
+	if (!user) {
+		return (
+			<div className="userInfoPage">
+				<h2>UserInfoPage</h2>
+
+				<div className="users">
+					<p>пользователя таким userId нет</p>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="userInfoPage">
+			<h2>UserInfoPage</h2>
+
+			<div className="users">
+				<p>{user.jobTitle}</p>
+				<p>{user.email}</p>
+				<img src={user.avatar} alt="" width={200} height={200} />
+				<p>{user.fullName}</p>
+				<p>{user.bio}</p>
+			</div>
+		</div>
+	);
+}
